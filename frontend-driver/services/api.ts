c59cafa2f0274981bb844938964da672be168ce8
PrathamMMallya <lathaprathamnayak@gmail.com>
2025-06-07 20:26:35 +0530

    testing

--- frontend-driver/services/api.ts
+++ frontend-driver/services/api.ts
@@ -1,9 +1,9 @@
-// import { Platform } from 'react-native';
+import { Platform } from 'react-native';
 //Use if you havent tunneled/hosted your backend
 // const HOST = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
 // export const BASE_API_URL = `http://${HOST}:8000/api/driver/`;
 // export const SOCKET = `ws://${HOST}:8000/ws/driver/`;
-
+// Pratham is Testing
 //Use this if you have tunneled or hosted backend
 const HOST = "cub-true-shiner.ngrok-free.app"
 // https://cub-true-shiner.ngrok-free.app/driver
